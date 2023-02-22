6954b68c57c1b0d4f2920076c0eba33f05f522cc
Nicolas Bru Frantzen <nicolas.bru.frantzen@gmail.com>
2023-02-22 21:58:30 +0100

    feat: add token parsing in get_user_info example

--- examples/get_user_info.rs
+++ examples/get_user_info.rs
@@ -1,14 +1,24 @@
 use saxobank_rs::client::OpenAPIClient;
 
+use clap::{arg, command};
 use std::error::Error;
 
-const TOKEN: &str = "";
+fn get_token() -> String
+{
+    let matches = command!()
+        .arg(arg!(
+            -t --token <TOKEN> ... "OpenAPI token to run example with"
+        ).required(true))
+        .get_matches();
+
+    matches.get_one::<String>("token").unwrap().clone()
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>>
 {
-    let client = OpenAPIClient::new_sim(TOKEN);
-
+    let client = OpenAPIClient::new_sim(&get_token());
     println!("{:?}", client.get_user_info().await?);
+
     Ok(())
 }
