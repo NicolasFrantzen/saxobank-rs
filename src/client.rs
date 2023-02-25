665591dacda80443d20df766043b73c21fb93d53
Nicolas Bru Frantzen <nicolas.bru.frantzen@gmail.com>
2023-02-25 20:39:57 +0100

    chore: remove extra v1 module

--- src/client.rs
+++ src/client.rs
@@ -142,12 +142,12 @@ impl<S: HttpSend> OpenAPIClient<S> {
         }
     }
 
-    pub async fn get_user_info<'a>(&self) -> Result<port::v1::users::Response, OpenAPIError> {
-        self.get(port::v1::users::Request("me")).await
+    pub async fn get_user_info<'a>(&self) -> Result<port::users::Response, OpenAPIError> {
+        self.get(port::users::Request("me")).await
     }
 
-    pub async fn get_client_info<'a>(&self) -> Result<port::v1::clients::Response, OpenAPIError> {
-        self.get(port::v1::clients::Request("me")).await
+    pub async fn get_client_info<'a>(&self) -> Result<port::clients::Response, OpenAPIError> {
+        self.get(port::clients::Request("me")).await
     }
 }
 
@@ -168,7 +168,7 @@ mod tests {
         let response =
             reqwest::Response::from(http::Response::builder().status(200).body("{}").unwrap());
         let api_response =
-            OpenAPIClient::<Sender>::parse_response::<port::v1::users::Request>(response).await;
+            OpenAPIClient::<Sender>::parse_response::<port::users::Request>(response).await;
 
         #[cfg(debug_assertions)]
         dbg!(&api_response);
@@ -183,7 +183,7 @@ mod tests {
         let response =
             reqwest::Response::from(http::Response::builder().status(401).body("{}").unwrap());
         let api_response =
-            OpenAPIClient::<Sender>::parse_response::<port::v1::users::Request>(response).await;
+            OpenAPIClient::<Sender>::parse_response::<port::users::Request>(response).await;
 
         #[cfg(debug_assertions)]
         dbg!(&api_response);
@@ -209,7 +209,7 @@ mod tests {
                 .unwrap(),
         );
         let api_response =
-            OpenAPIClient::<Sender>::parse_response::<port::v1::users::Request>(response).await;
+            OpenAPIClient::<Sender>::parse_response::<port::users::Request>(response).await;
 
         #[cfg(debug_assertions)]
         dbg!(&api_response);
